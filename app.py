b725be5967b5bf50fe58de1b3afa5db88ff84a9e
Ehsan Shokrgozar <ehsan.shokrgozar@autodesk.com>
2020-12-01 03:12:47 -0800

    Switched to using support_url from tk-core instead of the hard-coded email address

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ class LaunchPublish(Application):
                 "current value is '%s' - please double check that this path "
                 "is valid and update as needed in this app's configuration. "
                 "If you have any questions, don't hesitate to contact support "
-                "on support@shotgunsoftware.com." % app_path
+                "via %s" % (app_path, sgtk.support_url,)
             )
 
     def launch_publish(self, entity_type, entity_ids):
